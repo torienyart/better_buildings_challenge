4548cf9e68b7a41cac8d6f5be4d64b4bbaa0aa07
Tori Enyart <torienyart@gmail.com>
2023-02-06 16:39:12 -0700

    Add Partners link to every page

---
+++ spec/features/application/application_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'application view' do
+  it 'shows the partners index page link on every page' do
+    sector_1 = Sector.create!(sector_name:'Data Centers', sufficient_staff: true, funding: 1000000)
+    partner_1 = Partner.create!(name:'Intuit', goal_achiever: false, sector_id: sector_1.id, btu_22: 534789025)
+
+    visit '/sectors'
+
+    expect(page).to have_link('Partners')
+
+    visit "/sectors/#{sector_1.id}"
+
+    expect(page).to have_link('Partners')
+
+    visit '/partners'
+
+    expect(page).to have_link('Partners')
+
+    visit "/partners/#{partner_1.id}"
+
+    expect(page).to have_link('Partners')
+  end
+end
